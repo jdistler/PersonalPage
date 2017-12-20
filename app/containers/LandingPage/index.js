87c944b92612e4adf5357d3d66c9c10a11ade390
Joseph Distler <jdistler@utexas.edu>
2017-12-19 22:57:14 -0600

    Remove phone from landing page

--- app/containers/LandingPage/index.js
+++ app/containers/LandingPage/index.js
@@ -33,7 +33,6 @@ export default class FeaturePage extends React.Component { // eslint-disable-lin
         <LargeIcon>
         <a href="#" title="Resume"><I className="fa fa-file-text-o"></I></a>
         <a href="mailto:jdistler@utexas.edu"  title="jdistler@utexas.edu"><I className="fa fa-envelope-o"></I></a>
-        <a href="tel:+1-201-367-8707" title="201-367-8707"><I className="fa fa-phone"></I></a>
         <a href="https://www.linkedin.com/in/josephdistler/" title="https://www.linkedin.com/in/josephdistler/"><I className="fa fa-linkedin"></I></a>
         <a href="https://github.com/jdistler" title="https://github.com/jdistler"><I className="fa fa-github"></I></a>
         </LargeIcon>
